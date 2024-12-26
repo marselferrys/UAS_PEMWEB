6f712d949cc3695f58f3562593a9ee5ad23a774e
121140195_Marchel Ferry Timoteus S <66813668+marselferrys@users.noreply.github.com>
2024-12-26 15:08:35 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,34 @@
 <?php
-//Membuat koneksi ke database
-$mysqli = mysqli_connect("localhost","root","root","students");
+class Database {
+    private $host = "xxx"; // Hostname atau alamat server database (contoh: localhost, IP address, atau domain
+    private $username = "xxx";  // Username untuk autentikasi ke database (biasanya root untuk localhost)
+    private $password = "xxx";  // Password yang digunakan untuk autentikasi username
+    private $database = "students"; // Nama database yang ingin diakses
+    public $connection; // Properti publik untuk menyimpan koneksi database
 
-if (!$mysqli) {
+    // Constructor untuk menginisialisasi koneksi
+    public function __construct() {
+        $this->connect();
+    }
 
-die("Connection error: " . mysqli_connect_error());
+    // Metode untuk membuat koneksi ke database
+    private function connect() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
 
+        if ($this->connection->connect_error) {
+            die("Connection error: " . $this->connection->connect_error);
+        }
+    }
+
+    // Metode untuk menutup koneksi (opsional)
+    public function close() {
+        if ($this->connection) {
+            $this->connection->close();
+        }
+    }
 }
+
+// Membuat instance dari class Database
+$db = new Database();
+$mysqli = $db->connection; // Mengakses koneksi
 ?>
